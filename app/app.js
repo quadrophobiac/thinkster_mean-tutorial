4ad1831e9206db3d13a5c3f8233e297ecc3b1431
stephenfortune <stephen.j.fortune@gmail.com>
2015-02-27 00:55:47 +0000

    did a staright copy and paste because whatever I had written previously was not rendering in localhost

--- app/app.js
+++ app/app.js
@@ -1,15 +1,16 @@
 'use strict';
+var app = angular.module('flapperNews', []);
 
-// Declare app level module which depends on views, and components
-var app = angular.module('flapperNews', ['ngRoute']);
+app.controller('MainCtrl', [
+  '$scope',
+  function($scope){
+    $scope.test = 'Hello world!';
 
-app.controller('MainCtrl', ['$scope', function($scope){
-  $scope.test = 'Hello World';
-  $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
-  ];
-}]);
+    $scope.posts = [
+      'post 1',
+      'post 2',
+      'post 3',
+      'post 4',
+      'post 5'
+    ];
+  }]);
