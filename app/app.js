0a10325ce91f25fd062ef3385b39ddf7eb940aed
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-03 00:52:25 +0000

    migration of data storing structures in file

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
 'use strict';
 var app = angular.module('flapperNews', []);
 
-app.factory('posts', [function(){}]);
+app.factory('posts', [function(){
+  var o = {
+    posts:[]
+  };
+  return o;
+}]);
 
 
 app.controller('MainCtrl', [
   '$scope',
-  function($scope){
+  'posts',
+  function($scope, posts){
     $scope.test = 'Hello world!';
 
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
+    $scope.posts = posts.posts;
 
     $scope.addPost = function(){
       // catch null entries
