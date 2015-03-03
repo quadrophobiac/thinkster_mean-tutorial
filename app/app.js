ccf10a47975906aa845ff987772050275e5d8c4a
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-03 00:30:28 +0000

    example view and controller updates to include hyperlinks

--- app/app.js
+++ app/app.js
@@ -20,8 +20,13 @@ app.controller('MainCtrl', [
         return;
       }
 
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
 
     $scope.incrementUpvotes = function(post) {
