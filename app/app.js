a0df65536884528591fca94a80b76567eb705259
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-04 01:28:59 +0000

    the posts template is not rendering the title and URL correctly

--- app/app.js
+++ app/app.js
@@ -49,6 +49,16 @@ app.controller('PostsCtrl', [
 
       $scope.post = posts.posts[$stateParams.id];
 
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
+
     }
 ]);
 
