cacc3a502c57ff204ad9cd2c43d600576fd360ae
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-06 19:45:41 +0000

    addition of another model, with requirement function arguments completed

--- flapper-news/models/Comments.js
+++ flapper-news/models/Comments.js
@@ -4,6 +4,11 @@
 
 var mongoose = require('mongoose');
 
-var CommentSchema = new mongoose.Schema({});
+var CommentSchema = new mongoose.Schema({
+    body: String,
+    author: String,
+    upvotes: {type: Number, default: 0},
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post'}
+});
 
 mongoose.model('Comment', CommentSchema);
