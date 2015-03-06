a8d2ecd8784f9f5e7a4561cdfb4d89572a929b4c
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-06 19:39:05 +0000

    attributes for this tutorials model added

--- flapper-news/models/Posts.js
+++ flapper-news/models/Posts.js
@@ -4,6 +4,11 @@
 
 var mongoose = require('mongoose');
 
-var PostSchema = new mongoose.Schema({});
+var PostSchema = new mongoose.Schema({
+    title: String,
+    link: String,
+    upvotes: {type: Number, default: 0},
+    comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+});
 
-mongoose.model('Post', PostSchema);h
+mongoose.model('Post', PostSchema);
