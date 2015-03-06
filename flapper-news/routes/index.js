29846f448a76e2496fbb90f57124087e07787217
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-06 19:52:09 +0000

    SO routes are defined in a syntax style that is closer to the convuluted javascript I am accustomed to, rather than the stylistic conventions employed in earlier parts of this tutorial

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -7,3 +7,16 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/*GET: view all posts */
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.Model('Comment');
+
+router.get('/posts', function(req, res, next){
+  Post.find(function(err, posts){
+    if(err){ return next(err);}
+    res.json(posts);
+  });
+});
