db414f0edaa66e04e57b6b445540cd5b7c64bf79
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-08 23:57:45 +0000

    making some edits to index file in order that the sequence of router methods makes the most sense

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var mongoose = require('mongoose');
-var Post = mongoose.model('Post');
-var Comment = mongoose.model('Comment');
-
 /*GET: view all posts */
-
-
 //
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
