cca4a60ecc5226d6f488dc1dd0582d646894d5d3
stephenfortune <stephen.j.fortune@gmail.com>
2015-03-08 23:55:15 +0000

    server will load mongoDB which can save posts correctly

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -8,31 +8,31 @@ router.get('/', function(req, res, next) {
 
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
-//var Comment = mongoose.model('Comment');
+var Comment = mongoose.model('Comment');
 
 /*GET: view all posts */
 
 
 //
-//router.get('/posts', function(req, res, next) {
-//  Post.find(function(err, posts){
-//    if(err){ return next(err); }
-//
-//    res.json(posts);
-//  });
-//});
-//
-///* POST: add data to database */
-//
-//router.post('/posts', function(req, res, next){
-//  var post = new Post(req.body);
-//
-//  post.save(function(err, post){
-//    if(err){ return next(err); }
-//
-//    res.json(post);
-//  });
-//
-//});
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
+
+/* POST: add data to database */
+
+router.post('/posts', function(req, res, next){
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
+
+});
 
 module.exports = router;
